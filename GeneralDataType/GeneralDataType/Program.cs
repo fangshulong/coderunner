94d89e0ab23f15a9dd17f7eba6426d6513642446
fangshulong <fangshulong@androidesk.com>
2013-09-24 23:07:50 +0800

    add is type

--- GeneralDataType/GeneralDataType/Program.cs
+++ GeneralDataType/GeneralDataType/Program.cs
@@ -12,7 +12,7 @@ namespace GeneralDataType
 			// out = 2
 			Console.WriteLine (numbers.Length);
 
-			String[,] citys = new String[2,2];
+			String[,] citys = new String[2, 2];
 			// out = 4
 			Console.WriteLine (citys.Length);
 
@@ -20,8 +20,8 @@ namespace GeneralDataType
 			for (int i = 0; i < 26; i ++) {
 
 				char[] bigAndSmall = new char[2];
-				bigAndSmall[0] = 'A';
-				bigAndSmall[1] = 'a';
+				bigAndSmall [0] = 'A';
+				bigAndSmall [1] = 'a';
 
 				letters [i] = new char[2];
 			}
@@ -36,21 +36,32 @@ namespace GeneralDataType
 
 			//as 
 			object[] arr = new object[2];
-			arr[0] = 123;
-			arr[1] = "test";
-			foreach (object temp in arr){
+			arr [0] = 123;
+			arr [1] = "test";
+			foreach (object temp in arr) {
 				try {
 					string s = (string)temp;
-					Console.WriteLine(s);
+					Console.WriteLine (s);
 				} catch (Exception e) {
-					Console.WriteLine(e.StackTrace);
+					Console.WriteLine (e.StackTrace);
 				}
 			}
 
-			for(int i = 0; i < arr.Length; i++) {
-				string s=arr[i] as string;
-				if( s!=null )Console.WriteLine( i+":"+s );
+			for (int i = 0; i < arr.Length; i++) {
+				string s = arr [i] as string;
+				if (s != null)
+					Console.WriteLine (i + ":" + s);
 			}
+
+			Object myObject = new Object (); 
+			Boolean b1 = (myObject is Object);      
+			Boolean b2 = (myObject is MainClass);
+
+			// out = b1 = True, b2 = False
+			Console.WriteLine ("b1 = " + b1 + ", b2 = " + b2);
+
+			//string multiWords = "I am a developer.";
+			//Console.WriteLine ("words num == " + multiWords.Split(" ").Length);
 		}
 	}
 }
